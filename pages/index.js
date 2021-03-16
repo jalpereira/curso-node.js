69992dd277993ebc638ca94fd0a4283ffd61a0f6
José Augusto de Lima Pereira <jalpereira@gmail.com>
2021-03-16 20:03:58 -0300

    Iniciando Dev

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,9 @@ function Home() {
     return (
         <div>
             <h2>Home</h2>
-            <Contador />
         </div>
     )
 }
 
-function Contador() {
-    const [Contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(Contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{Contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 
 export default Home
